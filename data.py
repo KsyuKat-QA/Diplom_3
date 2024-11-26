97a141520bb967b2185e809bbf339a6e77656e14
Ksenia Katiukhina <ksyusha_15@mail.ru>
2024-11-27 01:42:55 +0700

    test

--- data.py
+++
@@ -1,4 +0,0 @@
-browserName = 'chrome'
-
-LK_LOGIN = "asdfasdf451@mala.lakj"
-LK_PASS = "asdfasdf451@mala.lakj"
--- urls.py
+++
@@ -1,4 +0,0 @@
-ROOT_SITE = "https://stellarburgers.nomoreparties.site"         #главная страница
-LOGIN_SITE = ROOT_SITE+"/login"  #страница входа
-LENTA_SITE = ROOT_SITE+"/feed"   #Лента заказов
-ORDER_SITE = ROOT_SITE+"/account/order-history"
